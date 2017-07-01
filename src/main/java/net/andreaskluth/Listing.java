77bd002367bb53b8af93675d711dcc5c08944817
Andreas Kluth <mail@andreaskluth.net>
2017-07-01 22:39:35 +0200

    Not working ..

--- src/main/java/net/andreaskluth/Listing.java
+++ src/main/java/net/andreaskluth/Listing.java
@@ -10,13 +10,22 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 import javax.persistence.Version;
 
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.engine.spi.PersistentAttributeInterceptable;
+import org.hibernate.engine.spi.PersistentAttributeInterceptor;
 
 @Entity
-public class Listing {
+public class Listing implements PersistentAttributeInterceptable {
+
+    @Transient
+    private PersistentAttributeInterceptor interceptor;
 
     @Id
     private UUID id = UUID.randomUUID();
@@ -27,11 +36,12 @@ public class Listing {
     @Column(name = "store_id", nullable = true)
     private String storeId;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL }, optional = true)
+    @OneToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL }, optional = true)
     @JoinColumns(value = {
             @JoinColumn(name = "article_id", referencedColumnName = "article_id", insertable = false, updatable = false, nullable = true),
             @JoinColumn(name = "store_id", referencedColumnName = "store_id", insertable = false, updatable = false, nullable = true) })
     @NotFound(action = NotFoundAction.IGNORE)
+    @LazyToOne(LazyToOneOption.NO_PROXY)
     private Stock stock;
 
     @Version
@@ -62,11 +72,28 @@ public class Listing {
     }
 
     public Stock getStock() {
+        if (interceptor != null) {
+            return (Stock) interceptor.readObject(this, "stock", stock);
+        }
         return stock;
     }
 
     public void setStock(Stock stock) {
+        if (interceptor != null) {
+            this.stock = (Stock) interceptor.writeObject(this, "stock", this.stock, stock);
+            return;
+        }
         this.stock = stock;
+    }
+
+    @Override
+    public PersistentAttributeInterceptor $$_hibernate_getInterceptor() {
+        return interceptor;
+    }
+
+    @Override
+    public void $$_hibernate_setInterceptor(PersistentAttributeInterceptor interceptor) {
+        this.interceptor = interceptor;
     }
 
 }
